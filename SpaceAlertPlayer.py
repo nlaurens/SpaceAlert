84c24a86b64b24973a6ca26f4fc1cfa68aa5c5b9
Niels Laurens <nlaurens@gmail.com>
2013-09-16 12:16:17 +0200

    adds display and audio queue

--- SpaceAlertPlayer.py
+++ SpaceAlertPlayer.py
@@ -2,13 +2,50 @@
 import time
 import sys
 import mp3play
+from collections import deque
+from threading import Thread
+
 
 #TODO;
-# Vertaal tabel voor de zones, threats -> text & mp3!
+# Vertaal tabel voor de threats -> text & mp3!
+
+
+# Thread that handles the audioQueue and playback.
+def AudioThread(audioQ):
+    while True:
+        time.sleep(.1)
+
+        #Check if the audio queu needs processing
+        if len(audioQ) > 0:
+            sound = audioQ.popleft()
+            print 'playing audio: ' + sound
+            clip = mp3play.load('sounds/' + sound + '.mp3')
+            clip.play()
+            while clip.isplaying():
+                time.sleep(.1)
+
+
+# Thread that handles the display of messages and the clock!
+def DisplayThread(displayQ):
+    secondsPrinted = 0
+    while True:
+        time.sleep(.1)
+
+        # Check if we need to display the clock:
+        now = time.time()
+        minute, seconds = divmod(now - startTime, 60)
+        seconds = int(seconds)
+        if seconds != secondsPrinted:
+            secondsPrinted = seconds
+            print '%02d:%02d' % (minute, seconds)
+
+        # Check if the display queu needs processing:
+        if len(displayQ) > 0:
+            msg = displayQ.popleft()
+            print msg
+
 
 #Split the timeString (mmss) from the EventString (xxx)
-
-
 def splitEvent(event):
     i = 0
     while event[i].isdigit():
@@ -28,32 +65,39 @@ def convertTime(timeStr):
     sys.exit(1)
 
 
-def processEvent(event):
+def processEvent(event, audioQ, displayQ):
     if event['type'] == 'PE':
-        print 'Phase ends - ' + event['params']
+        print 'TODO Phase ends - ' + event['params']
     elif event['type'] == 'AL':
         phase = event['params'][0]
         zone = event['params'][-1]
         threat = event['params'][1:len(event['params']) - 1]
-        event['text'] = 'Alert - T%d - threat: %s - zone:%s' % (int(phase), threat, zone)
-        event['sound'] = r'sounds\alert.mp3'
+        msg = 'Alert - T%d - threat: %s - zone:%s' % (int(phase), threat, zone)
+        displayQ.append(msg)
+        audioQ.append('alert')
+        audioQ.append('time_t_plus_'+ phase)
+        audioQ.append('threat')
+        audioQ.append('zone_'+ zone)
+        audioQ.append('repeat')
+        audioQ.append('time_t_plus_'+ phase)
+        audioQ.append('threat')
+        audioQ.append('zone_'+ zone)
     elif event['type'] == 'UR':
-        print 'Unconfirmed Report - ' + event['params']
+        print 'TODO Unconfirmed Report - ' + event['params']
     elif event['type'] == 'ID':
-        print 'Incoming Data' + event['params']
+        print 'TODO Incoming Data' + event['params']
     elif event['type'] == 'DT':
-        print 'Data Transfer' + event['params']
+        print 'TODO Data Transfer' + event['params']
     elif event['type'] == 'CS':
-        print 'Communications Down' + event['params']
+        print 'TODO Communications Down' + event['params']
 
     return event
 
-script = '002AL1STB,005AL2TB,215DT,300DT,310AL3TR,320ID,410PE1,425DT,630ID,720PE2'
+script = '015AL1STB,100AL2TB,215DT,300DT,310AL3TR,320ID,410PE1,425DT,630ID,720PE2'
 
 filename = r'sounds\alert.mp3'
 
 lijst = script.split(',')
-print script
 
 eventList = []
 for eventStr in lijst:
@@ -62,6 +106,7 @@ for eventStr in lijst:
     event['time'] = convertTime(timeStr)
     event['type'] = eventStr[0:2]
     event['params'] = eventStr[2:]
+    event['sounds'] = []
     eventList.append(event)
 
 #spawn t-1m, t-20s events for end of phase:
@@ -86,18 +131,33 @@ for event in eventList:
 #Sort the list after the inserts
 eventList = sorted(eventList, key=lambda k: k['time'])
 
+# Initilaize the audio & display queu:
+audioQ = deque(['begin_first_phase'])
+displayQ = deque(['Alert, enemy activity detected! Please begin first phase!'])
+
+# Spawn the audio thread:
+audioThread = Thread(target = AudioThread, args = (audioQ, ))
+audioThread.setDaemon(True)
+audioThread.start()
+
+# Spawn the display thread:
+displayThread = Thread(target = DisplayThread, args = (displayQ,))
+displayThread.setDaemon(True)
+displayThread.start()
 
 # Run the game!
 startTime = time.time()
 for event in eventList:
+
+    # Set the timer for the next event.
     timer = startTime + event['time']
     now = time.time()
+
+    # Wait for next event.
     while now < timer:
-        time.sleep(1)
+        # go easy on the CPU:
+        time.sleep(.1)
         now = time.time()
-        minute, seconds = divmod(now - startTime, 60)
-        print '%02d:%02d' % (minute, seconds)
-    event = processEvent(event)
-    print event['text']
-    mp3 = mp3play.load(event['sound'])
-    mp3.play()
+
+    # Process & run the event
+    event = processEvent(event, audioQ, displayQ)
