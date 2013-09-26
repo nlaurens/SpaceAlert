c018f16c459a12352c7b1dd0545d51fe325a9e63
Niels Laurens <nlaurens@gmail.com>
2013-09-26 08:47:37 +0200

    wrote todo in missionList

---
+++ missionList.py
@@ -0,0 +1,6 @@
+# Load the missions from a ini/jaml-file into a list
+# Optional: Evaluate all misions with the script parser to detect mistakes
+#    before running such a mission!
+# Hand the mission list back to a simple menu that allows you to choose from
+# the list which mission to play.
+
