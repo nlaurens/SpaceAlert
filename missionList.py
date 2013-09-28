e38845cefc196ce31a665010a7497cf07f159f5d
Niels Laurens <nlaurens@gmail.com>
2013-09-28 18:02:32 +0200

    adds a missionList reader that reads mission scripts from a config file

--- missionList.py
+++ missionList.py
@@ -1,6 +1,26 @@
-# Load the missions from a ini/jaml-file into a list
-# Optional: Evaluate all misions with the script parser to detect mistakes
-#    before running such a mission!
-# Hand the mission list back to a simple menu that allows you to choose from
-# the list which mission to play.
+import ConfigParser
 
+
+class missionList():
+
+    def __init__(self):
+        #TODO
+        #Scan for all cfg files in the mission dir
+        #TMP solution, should be replaced with all .cfg files from 'mission' dir.
+        self.missionConfigs = ['duckling.cfg']
+
+    def parseConfigFiles(self):
+        missionList = []
+
+        config = ConfigParser.RawConfigParser()
+        config.read('missions/duckling.cfg')
+
+        for section in config.sections():
+            options = config.options(section)
+            missions = []
+            for option in options:
+                missions.append({option: config.get(section, option)})
+            missionList.append({section: missions})
+
+        #TODO: CHECK IF ALL MISSIONS ARE PLAYBLE.
+        return missionList
